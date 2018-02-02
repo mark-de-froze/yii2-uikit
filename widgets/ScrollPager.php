ecf7895557ea2233c1bfbb396f4412a4fe7f4a2a
Mark de Froze <mark.de.froze@gmail.com>
2018-02-02 14:58:33 +0200

    fixs

--- widgets/ScrollPager.php
+++ widgets/ScrollPager.php
@@ -9,7 +9,7 @@ class ScrollPager extends \kop\y2sp\ScrollPager
     public $container = '.grid-view tbody';
     public $item = 'tr';
     public $triggerTemplate = '<tr class="ias-trigger" style="background: none; border: none; height: 0;"><td colspan="100%" class="uk-text-center"><div class="uk-position-bottom-center" style="bottom: -50px;"><a class="uk-button uk-button-secondary">{text}</a></div></td></tr>';
-    public $spinnerTemplate = '<tr class="ias-spinner"><td colspan="100%" class="uk-text-center"><div uk-spinner></div></td></tr>';
+    public $spinnerTemplate = '<div class="ias-spinner uk-position-absolute uk-width-1-1"><div class="uk-position-center"><div uk-spinner></div></div></div>';
     public $noneLeftText;
     public $triggerText;
     public $noneLeftTemplate = '<tr class="ias-noneleft"><td colspan="100%" class="uk-text-center uk-h3">{text}</td></tr>';
